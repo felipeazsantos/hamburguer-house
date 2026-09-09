90967562d34777134809fcfa926f59add4b9deea
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-09 15:16:52 -0300

    feat: create login component

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Header from "./components/Header.tsx";
+import Login from "./Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
-    <App />
+
+    {/* <Header /> */}
+    <Login />
+    {/* <App /> */}
   </StrictMode>,
 );
