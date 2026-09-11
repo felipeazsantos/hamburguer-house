c056162d1dffab4a9df9dc5812ab7cececfd5ad9
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-11 16:00:55 -0300

    feat: express test

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.get("/", () => {});
+app.get("/", (req, res) => {
+  res.json("teste");
+});
 
-app.listen(3000, () => {console.log("Servidor rodando na porta 3000")});
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
