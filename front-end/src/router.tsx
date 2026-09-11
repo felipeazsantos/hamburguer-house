41990b99723e9df17b109d25de29d1c2bcc87346
Felipe Azevedo <felipeasantos10@hotmail.com>
2026-09-11 15:19:18 -0300

    feat: create layout component

--- front-end/src/router.tsx
+++ front-end/src/router.tsx
@@ -1,12 +1,27 @@
+import Header from "./components/Header.tsx";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
   {
     path: "/login",
